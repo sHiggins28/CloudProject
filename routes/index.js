cd8b83e7bd3783376f5db25b3242b5348141c0cd
shane <G00339730@gmit.ie>
2020-03-21 17:41:43 +0000

    First page basic func query

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,19 @@ var userInfoSchema = new Schema({
   email: String,
   phone: String,
   sex: String,
-  city: String
+  city: String,
+  cars: String,
+  nature: String,
+  movies: String,
+  music: String,
+  books: String,
+  sport: String,
+  travelling: String,
+  gaming: String,
+  flowers: String,
+  dance: String,
+  technology: String,
+  outdoor_activities: String
 });
 
 var userInfo = mongoose.model('UserInfos', userInfoSchema);
@@ -39,7 +51,19 @@ router.post('/userinfo', function (req, res, next) {
     email: req.body.email,
     phone: req.body.phone,
     sex: req.body.sex,
-    city: req.body.city
+    city: req.body.city,
+    cars: req.body.cars,
+    nature: req.body.nature,
+    movies: req.body.movies,
+    music: req.body.music,
+    books: req.body.books,
+    sport: req.body.sport,
+    travelling: req.body.travelling,
+    gaming: req.body.gaming,
+    flowers: req.body.flowers,
+    dance: req.body.dance,
+    technology: req.body.technology,
+    outdoor_activities: req.body.outdoor_activities
   }
   console.log(req.body);
   console.log(data);
@@ -47,14 +71,14 @@ router.post('/userinfo', function (req, res, next) {
   const userInfoModel = new userInfo(data);
   userInfoModel.save();
 
-  // userInfo.find({city:{$eq:"Tuam"}})
-  //     .then(function(doc) {
-  //       console.log("Got as far as here, array length is: " + doc.length);
-  //       res.render('result1', {title: "My Page", items: doc});
-  //     });
+  userInfo.find({$and: [{city:{$eq:req.body.city}}, {sport:{$eq:req.body.sport}}, {movies:{$eq:req.body.movies}}] })
+      .then(function(doc) {
+        console.log("Got as far as here, array length is: " + doc.length);
+        res.render('result1', {title: "My Page", items: doc});
+      });
 
 
-  res.render('interests');
+  //res.render('interests');
 });
 
 router.get('/userinfo',  function(req, res, next) {
